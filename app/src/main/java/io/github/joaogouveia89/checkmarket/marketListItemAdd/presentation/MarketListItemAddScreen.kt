85c400eb17284a3e6af884d0aa549fe1f3dae1b0
Joao Gouveia <joao.gouveia@boost-it.pt>
2024-08-07 13:09:50 -0300

    added no query typed message

--- app/src/main/java/io/github/joaogouveia89/checkmarket/marketListItemAdd/presentation/MarketListItemAddScreen.kt
+++ app/src/main/java/io/github/joaogouveia89/checkmarket/marketListItemAdd/presentation/MarketListItemAddScreen.kt
@@ -1,43 +1,62 @@
 package io.github.joaogouveia89.checkmarket.marketListItemAdd.presentation
 
-import android.annotation.SuppressLint
-import android.util.Log
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import io.github.joaogouveia89.checkmarket.R
-import io.github.joaogouveia89.checkmarket.core.presentation.topBars.CheckMarketAppBar
 import io.github.joaogouveia89.checkmarket.core.presentation.topBars.CheckMarketSearchAppBar
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MarketListiTemAddScreen(
     onNavigateBack: () -> Unit
 ) {
+    var query = "" // TEMPORARY WHILE THE VIEW MODEL IS NOT IMPLEMENTED
     // TODO: ADD A BACK FUNCTIONALITY TO THE APPBAR
     Scaffold(
         topBar = {
             // Top App Bar
             CheckMarketSearchAppBar(
                 onBackClick = { onNavigateBack() },
-                onQueryChange = {/* TODO Call view model */}
+                onQueryChange = {/* TODO Call view model */ }
             )
         }
     ) { paddingValues ->
-//        MarketListItemAddContent(
-//            modifier = Modifier
-//                .fillMaxHeight(),
-//            paddingValues = paddingValues,
-//            onNavigateBack = onNavigateBack
-//        )
+        Box(
+            Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+//            MarketListItemAddContent(
+//                modifier = Modifier
+//                    .fillMaxHeight(),
+//                paddingValues = paddingValues,
+//                onNavigateBack = onNavigateBack
+//            )
+            if (query.isEmpty()) {
+                NoQueryTypedMessage()
+            }
+        }
     }
+}
+
+@Composable
+fun NoQueryTypedMessage() {
+    Text(
+        color = Color.Black,
+        text = "Use the search bar above to look for an item to add"
+    )
 }
 
 @Preview
 @Composable
 private fun MarketListItemAddScreenPreview() {
-    MarketListiTemAddScreen{}
+    MarketListiTemAddScreen {}
 }
 
